201087f85c02d22876390cab7ccad6916a7d50eb
Yashveer Bika <yashveerbika@gmail.com>
2023-02-04 16:47:31 -0600

    basic NavBar with dead links

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Navbar } from 'react-bootstrap';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar></NavBar>
     </div>
   );
 }
