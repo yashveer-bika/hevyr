610e8fe1675e78db9c4114383480b60ff70212b7
Yashveer Bika <yashveerbika@gmail.com>
2023-03-01 10:57:36 -0600

    add simple timer logic. clean up stopwatch code

--- src/components/Log/Stopwatch.tsx
+++ src/components/Log/Stopwatch.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import { useStopwatch } from 'react-timer-hook';
 
 export default function Stopwatch() {
@@ -13,16 +14,74 @@ export default function Stopwatch() {
     reset,
   } = useStopwatch({ autoStart: false });
 
+  function onReset() {
+      reset();
+      pause();
+  }
+
+  let buttonDisplay : JSX.Element;
+
+  if (!isRunning && seconds==0 && minutes==0 && hours==0 && days==0) {
+    buttonDisplay = 
+        <div className="clock-button-display"> 
+            <Button onClick={() => isRunning ? null : start()}>Start</Button> 
+        </div>
+  } else if (isRunning) {
+      buttonDisplay = 
+        <div className="clock-button-display"> 
+            <Button onClick={pause}>Stop</Button>  
+        </div>
+  } else {
+    buttonDisplay = 
+        <div className="clock-button-display"> 
+            <Button onClick={ () => onReset() }>Reset</Button> 
+            <Button onClick={() => isRunning ? null : start()}>Start</Button>
+        </div>
+  }
+
+  function addLeadingZero(x : number) {
+    return (x < 10) ? `0${x}` : x
+  }
+
+  let timeDisplay : JSX.Element;
+  if (days < 1 && hours < 1) {
+      timeDisplay = 
+        <div className="clock-time-display"> 
+            <span>{addLeadingZero(minutes)}</span>:<span>{addLeadingZero(seconds)}</span> 
+        </div>
+  } else if (days < 1) {
+      timeDisplay = 
+        <div className="clock-time-display"> 
+            <span>{addLeadingZero(hours)}</span>:<span>{addLeadingZero(minutes)}</span>:<span>{addLeadingZero(seconds)}</span> 
+        </div>
+  } else {
+      timeDisplay = 
+        <div className="clock-time-display"> 
+            <span>{addLeadingZero(days)}</span>:<span>{addLeadingZero(hours)}</span>:<span>{addLeadingZero(minutes)}</span>:<span>{addLeadingZero(seconds)}</span> 
+        </div>
+  }
+
+
+  // TODO: add case when timer goes above 1 hour
 
   return (
     <div style={{textAlign: 'center'}}>
       <div style={{fontSize: '100px'}}>
-        <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+        {/* <span>{addLeadingZero(minutes)}</span>:<span>{addLeadingZero(seconds)}</span> */}
+        {timeDisplay}
       </div>
-      <p>{isRunning ? 'Running' : 'Not running'}</p>
-      <button onClick={start}>Start</button>
-      <button onClick={pause}>Pause</button>
-      <button onClick={() => reset()}>Reset</button>
+
+      {/* <p>{isRunning ? 'Running' : 'Not running'}</p> */}
+
+      {buttonDisplay}
+
+      {/* 
+      <Button onClick={() => isRunning ? null : start()}>Start</Button>
+      <Button onClick={pause}>Pause</Button>
+      <Button onClick={ () => onReset() }>Reset</Button> 
+      */}
+
+
     </div>
   );
 }
