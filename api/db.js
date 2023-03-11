d519e85405505cf4d34c3875e75149bf7312c678
Yashveer Bika <yashveerbika@gmail.com>
2023-03-11 16:06:43 -0600

    api setup with typescript, express

--- api/db.js
+++
@@ -1,4 +0,0 @@
-var mongoose = require('mongoose');
-const password = "4nfw2aey"
-const username = "yashveerbika"
-mongoose.connect(`mongodb+srv://${username}:${password}@hevy-clone.nnu3be0.mongodb.net/?retryWrites=true&w=majority`);
